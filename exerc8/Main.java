482250b60d946784a9b3c2b88a800fb3850b2229
SyphonFilter2003 <164701384+SyphonFilter2003@users.noreply.github.com>
2024-03-27 09:33:04 -0300

    Add files via upload

---
+++ exerc8/Main.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Pizza margherita = new Pizza("Margherita", 25.0, Arrays.asList("Molho de tomate", "Muçarela", "Manjericão"));
+        Pizza pepperoni = new Pizza("Pepperoni", 30.0, Arrays.asList("Molho de tomate", "Muçarela", "Pepperoni"));
+
+        Pedido pedido = new Pedido();
+        pedido.setEnderecoEntrega(solicitarEndereco(scanner));
+
+        boolean continuarPedido = true;
+        while (continuarPedido) {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1. Margherita");
+            System.out.println("2. Pepperoni");
+            System.out.println("3. Montar pizza personalizada");
+            System.out.println("0. Finalizar pedido");
+
+            int opcao = scanner.nextInt();
+            switch (opcao) {
+                case 1:
+                    pedido.adicionarPizza(margherita);
+                    break;
+                case 2:
+                    pedido.adicionarPizza(pepperoni);
+                    break;
+                case 3:
+                    Pizza pizzaPersonalizada = montarPizzaPersonalizada(scanner);
+                    pedido.adicionarPizza(pizzaPersonalizada);
+                    break;
+                case 0:
+                    continuarPedido = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+
+        pedido.fornecerDetalhesPedido();
+        System.out.println("Valor total a ser pago: " + pedido.calcularValorTotalPizzas());
+
+        scanner.close();
+    }
+
+    private static String solicitarEndereco(Scanner scanner) {
+        System.out.print("Informe o endereço de entrega: ");
+        return scanner.next();
+    }
+
+    private static Pizza montarPizzaPersonalizada(Scanner scanner) {
+        System.out.print("Informe o nome da pizza personalizada: ");
+        String nomePizza = scanner.next();
+
+        System.out.print("Informe o valor da pizza personalizada: ");
+        double valorPizza = scanner.nextDouble();
+
+        System.out.print("Informe os ingredientes da pizza personalizada (separados por vírgula): ");
+        String ingredientesInput = scanner.next();
+        List<String> ingredientes = Arrays.asList(ingredientesInput.split(","));
+
+        return new Pizza(nomePizza, valorPizza, ingredientes);
+    }
+}
---
+++ exerc8/Pedido.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Pedido {
+    private List<Pizza> pizzas;
+    private String enderecoEntrega;
+    private boolean cancelado;
+
+    public Pedido() {
+        this.pizzas = new ArrayList<>();
+        this.cancelado = false;
+    }
+
+    public void adicionarPizza(Pizza pizza) {
+        pizzas.add(pizza);
+    }
+
+    public void fornecerDetalhesPedido() {
+        System.out.println("Pizzas no pedido:");
+        for (Pizza pizza : pizzas) {
+            System.out.println("- " + pizza.getNome() + " (" + pizza.getValor() + ")");
+        }
+        System.out.println("Endereço de entrega: " + enderecoEntrega);
+    }
+
+    public void cancelarPedido() {
+        this.cancelado = true;
+    }
+
+    public double calcularValorTotalPizzas() {
+        double total = 0.0;
+        for (Pizza pizza : pizzas) {
+            total += pizza.getValor();
+        }
+        return total;
+    }
+
+    // Getters e setters para os atributos
+    public String getEnderecoEntrega() {
+        return enderecoEntrega;
+    }
+
+    public void setEnderecoEntrega(String enderecoEntrega) {
+        this.enderecoEntrega = enderecoEntrega;
+    }
+
+    public boolean isCancelado() {
+        return cancelado;
+    }
+
+    // ...
+}
