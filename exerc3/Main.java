dfe0ef69a357593bfe2b68c041a9f670f3a53b5e
SyphonFilter2003 <164701384+SyphonFilter2003@users.noreply.github.com>
2024-03-27 11:05:54 -0300

    Main.java

--- exerc3/Main.java
+++ exerc3/Main.java
@@ -1,27 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<ContaBancaria> contas = new ArrayList<>();
 
-        System.out.print("Digite o número da conta: ");
-        int numeroConta = scanner.nextInt();
+        while (true) {
+            System.out.print("Digite o número da conta (ou -1 para sair): ");
+            int numeroConta = scanner.nextInt();
+            if (numeroConta == -1) {
+                break;
+            }
+            System.out.print("Digite o nome do titular da conta: ");
+            scanner.nextLine(); 
+            String nomeTitular = scanner.nextLine();
 
-        System.out.print("Digite o nome do titular da conta: ");
-        scanner.nextLine();
-        String nomeTitular = scanner.nextLine();
+            ContaBancaria minhaConta = new ContaBancaria(numeroConta, nomeTitular);
 
-        ContaBancaria minhaConta = new ContaBancaria(numeroConta, nomeTitular);
+            System.out.print("Digite o valor a ser depositado: ");
+            double valorDeposito = scanner.nextDouble();
+            minhaConta.depositar(valorDeposito);
 
-        System.out.print("Digite o valor a ser depositado: ");
-        double valorDeposito = scanner.nextDouble();
-        minhaConta.depositar(valorDeposito);
+            System.out.print("Digite o valor a ser sacado: ");
+            double valorSaque = scanner.nextDouble();
+            minhaConta.sacar(valorSaque);
 
-        System.out.print("Digite o valor a ser sacado: ");
-        double valorSaque = scanner.nextDouble();
-        minhaConta.sacar(valorSaque);
+            contas.add(minhaConta); 
+        }
 
-        minhaConta.exibirInformacoes();
-
+        System.out.println("\nInformações das contas:");
+        for (ContaBancaria conta : contas) {
+            conta.exibirInformacoes();
+        }
         scanner.close();
     }
 }
